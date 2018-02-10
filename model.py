1f3be9432133bdd5aef179f31389cc650e4e6a1a
なるみ <4680567+narumiruna@users.noreply.github.com>
2018-02-10 22:16:50 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,24 +1,24 @@
 from torch import nn
 
 class Generator(nn.Module):
-    def __init__(self, ch, normalization=nn.BatchNorm2d):
+    def __init__(self, ch):
         super(Generator, self).__init__()
 
         self.conv = nn.Sequential(
             nn.ConvTranspose2d(100, ch * 8, 4, 1, 0, 0, bias=False),
-            normalization(ch * 8, affine=True),
+            nn.InstanceNorm2d(ch * 8, affine=True),
             nn.ReLU(inplace=True),
 
             nn.ConvTranspose2d(ch * 8, ch * 4, 5, 2, 2, 1, bias=False),
-            normalization(ch * 4, affine=True),
+            nn.InstanceNorm2d(ch * 4, affine=True),
             nn.ReLU(inplace=True),
 
             nn.ConvTranspose2d(ch * 4, ch * 2, 5, 2, 2, 1, bias=False),
-            normalization(ch * 2, affine=True),
+            nn.InstanceNorm2d(ch * 2, affine=True),
             nn.ReLU(inplace=True),
 
             nn.ConvTranspose2d(ch * 2, ch, 5, 2, 2, 1, bias=False),
-            normalization(ch, affine=True),
+            nn.InstanceNorm2d(ch, affine=True),
             nn.ReLU(inplace=True),
 
             nn.ConvTranspose2d(ch, 3, 5, 2, 2, 1, bias=False),
