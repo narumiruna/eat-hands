377762219e8c3016da3c24324364252564698d8a
なるみ <4680567+narumiruna@users.noreply.github.com>
2018-02-11 10:54:53 +0800

    Update make_gif.py

--- make_gif.py
+++ make_gif.py
@@ -1,10 +1,8 @@
 import imageio
 import glob
 
-paths = sorted(glob.glob('images/samples*.jpg'))
+paths = sorted(glob.glob('wgan/samples*.jpg'))
 images = []
 for path in paths:
     images.append(imageio.imread(path))
-imageio.mimsave('images/sample.gif', images)
-
-
+imageio.mimsave('wgan/sample.gif', images)
